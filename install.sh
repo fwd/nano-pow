5f392d6908923e1cb3eb4c0f79f3e6a1101b80b5
Esteban <esteban@fwd.dev>
2022-08-22 15:21:56 -0400

    install

--- install.sh
+++ install.sh
@@ -6,29 +6,31 @@ curl https://sh.rustup.rs -sSf | sh
 source $HOME/.cargo/env
 
 # nano-work-server
-git clone https://github.com/nanocurrency/nano-work-server.git
-cd nano-work-server
+git clone https://github.com/nanocurrency/nano-work-server.git $HOME/nano-work-server
+cd $HOME/nano-work-server
 cargo build --release
+
+# $HOME/nano-work-server/target/release/nano-work-server --gpu 0:0
 
 # write out current crontab
 sudo crontab -l > cronjob
 #echo new cron into cron file
-echo "@reboot ~/nano-work-server/target/release/nano-work-server --gpu 0:0" >> cronjob
+echo "@reboot $HOME/nano-work-server/target/release/nano-work-server --gpu 0:0" >> cronjob
 #install new cron file
 sudo crontab cronjob
 rm cronjob
 
 if which node > /dev/null; then
-    echo "Node is installed. 👍🏽"
+echo "Node is installed. 👍🏽"
 else
-	curl -sL https://deb.nodesource.com/setup_16.x | sudo bash -
-	sudo apt -y install nodejs
-	npm i -g pm2
+curl -sL https://deb.nodesource.com/setup_16.x | sudo bash -
+sudo apt -y install nodejs
+npm i -g pm2
 fi
 
 # Server
-git clone https://github.com/fwd/nano-pow ./nano-work-proxy
-cd nano-work-proxy
+git clone https://github.com/fwd/nano-pow $HOME/nano-work-proxy
+cd $HOME/nano-work-proxy
 npm install
 
 npm install -g pm2
