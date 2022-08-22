dc6f3f9e6264b7d832f6a13790fc6bdc56593fe9
Esteban <esteban@fwd.dev>
2022-08-22 14:58:12 -0400

    readme

--- install.sh
+++ install.sh
@@ -1,15 +1,16 @@
 # OpenCL, GCC & Build Tools
 sudo apt install ocl-icd-opencl-dev gcc build-essential jq -y
+
 # Rust
 curl https://sh.rustup.rs -sSf | sh
 source $HOME/.cargo/env
 
-cd ~/
+# nano-work-server
 git clone https://github.com/nanocurrency/nano-work-server.git
 cd nano-work-server
 cargo build --release
 
-#write out current crontab
+# write out current crontab
 sudo crontab -l > cronjob
 #echo new cron into cron file
 echo "@reboot ~/nano-work-server/target/release/nano-work-server --gpu 0:0" >> cronjob
@@ -17,19 +18,21 @@ echo "@reboot ~/nano-work-server/target/release/nano-work-server --gpu 0:0" >> c
 sudo crontab cronjob
 rm cronjob
 
+if which node > /dev/null; then
+    echo "Node is installed. 👍🏽"
+else
+	curl -sL https://deb.nodesource.com/setup_16.x | sudo bash -
+	sudo apt -y install nodejs
+	npm i -g pm2
+fi
+
 # Server
-cd ~/
-git clone https://github.com/fwd/nano-pow
-cd nano-pow
+git clone https://github.com/fwd/nano-pow ./nano-work-proxy
+cd nano-work-proxy
 npm install
 
-cat <<EOT >> .env
-PORT=7080
-NAME=$(uuidgen | cut -f1 -d"-")
-SECRET=$(uuidgen | cut -f1 -d"-")
-EOT
-
 npm install -g pm2
-sudo pm2 start index.js --name nano-pow
+
+sudo pm2 start index.js --name nano-work-proxy
 sudo pm2 startup
-sudo m2 save
+sudo pm2 save
